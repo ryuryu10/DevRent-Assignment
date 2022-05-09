270185608f8c2931ef076deb52497c08a68cbb82
Seungwoo Yoo <ryuryu10@naver.com>
2022-05-10 00:46:44 +0900

    [ CREATE ] 게시글 상세 조회 기능 추가

--- settings/fourm/views.py
+++ settings/fourm/views.py
@@ -3,4 +3,8 @@ from .models import fourms
 
 def index(requset):
     article = fourms.objects.all()
-    return render(requset, 'pages/lists.html', {'article_list': article})
+    return render(requset, 'pages/article_index.html', {'article_list': article})
+
+def detail(request, article_id):
+    article = fourms.objects.get(id = article_id)
+    return render(request, 'pages/article_detail.html', {'article_detail': article})
