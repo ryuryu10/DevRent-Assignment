cf1e24b81640e781f8b784b7a56db1fb0116cebe
Seungwoo Yoo <ryuryu10@naver.com>
2022-05-09 22:46:16 +0900

    [ CREATE ] url 경로 제작

--- settings/settings/urls.py
+++ settings/settings/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from fourm import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('fourm/', views.index),
+    path('fourm/', include('fourm.urls')),
 ]
