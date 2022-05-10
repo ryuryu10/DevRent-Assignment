6158dc37e3a4e31de2d136b44a32e757e7339dde
Seungwoo Yoo <ryuryu10@naver.com>
2022-05-11 00:36:49 +0900

    [ CREATE ] 글 발행기능 추가

--- settings/fourm/urls.py
+++ settings/fourm/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:article_id>/', views.detail, name='detail'),
+    path('read/<int:article_id>/', views.detail, name='details'),
+    path('create/', views.create, name = 'create')
 ]
--- settings/fourm/views.py
+++ settings/fourm/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import fourms
+from .forms import WRITE_Form
 
 def index(requset):
     article = fourms.objects.all()
     return render(requset, 'pages/article_index.html', {'article_list': article})
 
 def detail(request, article_id):
-    article = get_list_or_404(fourms, pk=article_id)
+    article = get_object_or_404(fourms, id=article_id)
     return render(request, 'pages/article_detail.html', {'article_detail': article})
+
+def create(requset):
+    if requset.method == "POST":
+        form = WRITE_Form(requset.POST)
+        new_page = form.save()
+        return redirect('details', article_id=new_page.id)
+    else:
+        form = WRITE_Form()
+        return render(requset, 'pages/article_create.html', {'form':form})
